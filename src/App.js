e79b6a3fb5c628804b252b4309f8e7b55d3934c5
AntonioDomRos <antonio.dominguez@optimen.com.mx>
2023-07-21 11:45:53 -0600

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Container } from './components/Container';
+import { TaskCreator } from './components/TaskCreator';
+import { TaskTable } from './components/TaskTable';
+import { VisibilityControl } from './components/VisibilityControl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tasksItems, setTasksItems] = useState([]);
+
+	const [showCompleted, setShowCompleted] = useState(false);
+
+	function createNewTask(taskName) {
+		if (!tasksItems.find((task) => task.name === taskName)) {
+			setTasksItems([...tasksItems, { name: taskName, done: false }]);
+		}
+	}
+
+	useEffect(() => {
+		const data = localStorage.getItem('tasks');
+		if (data) {
+			setTasksItems(JSON.parse(data));
+		}
+	}, []);
+
+	const toggleTask = (task) => {
+		setTasksItems(
+			tasksItems.map((t) =>
+				t.name === task.name ? { ...t, done: !t.done } : t
+			)
+		);
+	};
+
+	const clearTask = () => {
+		setTasksItems(tasksItems.filter((task) => !task.done));
+		setShowCompleted(false);
+	};
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasksItems));
+	}, [tasksItems]);
+
+	return (
+		<main className="bg-dark vh-100 text-white">
+			<Container>
+				<TaskCreator createNewTask={createNewTask} />
+				<TaskTable tasks={tasksItems} toggleTask={toggleTask} />
+				<VisibilityControl
+					isChecked={showCompleted}
+					setShowCompleted={(checked) => setShowCompleted(checked)}
+					clearTask={clearTask}
+				/>
+				{showCompleted === true && (
+					<TaskTable
+						tasks={tasksItems}
+						toggleTask={toggleTask}
+						showCompleted={showCompleted}
+					/>
+				)}
+			</Container>
+		</main>
+	);
 }
 
 export default App;
